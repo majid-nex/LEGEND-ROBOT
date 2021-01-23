8ebf47d19d6428e3a857c456409533f07d20f74c
LEGEND X <74249238+LEGENDXOP@users.noreply.github.com>
2021-01-23 15:25:37 +0530

    Update chatbot.py

--- LEGEND/modules/chatbot.py
+++ LEGEND/modules/chatbot.py
@@ -136,7 +136,7 @@ def list_chatbot_chats(update: Update, context: CallbackContext):
 
 
 __help__ = f"""
-Chatbot utilizes the CoffeeHouse API and allows Ruhani to talk and provides a more interactive group chat experience.
+Chatbot utilizes the CoffeeHouse API and allows LEGEND BOT to talk and provides a more interactive group chat experience.
 
 *Commands:* 
 ➩ *Admins only:*
