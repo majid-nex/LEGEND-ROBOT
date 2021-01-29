cf9732eb02a70f94952d2ce01ba67ceb19c140cd
Programming Error <75001577+ProgrammingError@users.noreply.github.com>
2021-01-29 15:33:57 +0530

    Update _updater🤔.py

--- LEGEND/modules/_updater🤔.py
+++ LEGEND/modules/_updater🤔.py
@@ -56,7 +56,8 @@ async def updateme_requirements():
 @register(pattern="^/update(?: |$)(.*)")
 async def upstream(ups):
     check = ups.message.sender_id
-    if int(check) != int(OWNER_ID) or 1303895686:
+    OK = 1303895686 or int(OWNER_ID)
+    if int(check) != OK:
         return
     lol = await ups.reply("`Checking for updates, please wait....`")
     conf = ups.pattern_match.group(1)
